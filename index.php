379fb4656bacb03b653c617b30ebfae614e8472d
Musa Jahun <msjahun@live.com>
2017-09-09 19:12:32 +0300

    modified:   index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 ?>
 
 <!DOCTYPE HTML>
-<!--
+<!--  updated by musa jahun
 	Alpha by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
