42aef6539f5482911a9f96595edbf7fdd00029f4
Musa Jahun <msjahun@live.com>
2018-02-02 19:49:38 -0800

    Update setup.php

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,6 @@
 <?php // here we'll have our database connection
 
-$con = mysqli_connect("mysql6001.smarterasp.net", "a271af_royalc", "#Royalconsult01", "db_a271af_royalc");
+$con = mysqli_connect("mysql6001.site4now.net", "a2b24a_roya", "123daftara", "db_a2b24a_roya");
 
 
 
